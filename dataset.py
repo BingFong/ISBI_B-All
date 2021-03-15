43eb9d260aad3deae96365af351fcbe735fa6a70
Fong <karta379583@gmail.com>
2021-03-15 14:29:32 +0800

    fix: 修正驗證資料集(phase2)路徑 直接指定phase2資料夾為其路徑，避免搜尋到Phase3(test)資料夾

--- dataset.py
+++ dataset.py
@@ -9,23 +9,25 @@ from pathlib import Path
 import pandas as pd
 
 class get_data(Dataset):
-    def __init__(self, train_list, label, transform):
-        self.train_list = train_list
+    
+    def __init__(self, data_list, label, transform):
+        self.data_list = data_list
         self.label = label
         self.transform = transform
         
     def __getitem__(self, index):
-        img = Image.open(train_list[index])
+        img = Image.open(self.data_list[index])
 
         if self.transform is not None:
             img = self.transform(img)
-        # img = img.unsqueeze(0)
-        return img, label[index]
+            
+        return img, self.label[index]
         
     def __len__(self):
-        return len(train_list)
+        return len(self.data_list)
 
 def train_transform():
+    #compose training transformation
     return transforms.Compose([
         transforms.CenterCrop(300),
         transforms.RandomVerticalFlip(),
@@ -33,23 +35,23 @@ def train_transform():
         transforms.RandomAffine(degrees=360, translate=(0.2, 0.2)),
         transforms.Resize((224,224), Image.LANCZOS),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0, 0, 0],
-                             # std=[1, 1, 1])
     ])
 
 def valid_transform():
+    #compose validaing transformation
     return transforms.Compose([
         transforms.CenterCrop(300),
         transforms.Resize((224,224), Image.LANCZOS),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0, 0, 0],
-        #                      std=[1, 1, 1])
     ])
 
 def train_data():
+    #return training set directory list and labels
+    
     current_path = os.path.dirname(os.path.realpath(__file__))
     data_root = r'data'
-    train_list = glob.glob(os.path.join(current_path, data_root, '*', '*', '*.bmp'))
+    train_list = glob.glob(os.path.join(
+        current_path, data_root, 'fold_0', '*', '*.bmp'))
     
     label = []
     for data in train_list:
@@ -63,9 +65,11 @@ def train_data():
     return train_list, label
 
 def valid_data():
+    #return validating set directory list and labels
+    
     current_path = os.path.dirname(os.path.realpath(__file__))
     data_root = r'data'
-    valid_list = glob.glob(os.path.join(current_path, data_root, '*', '*.bmp'))
+    valid_list = glob.glob(os.path.join(current_path, data_root, 'phase2', '*.bmp'))
     phase2 = os.path.join(current_path, data_root, 'phase2.csv')
     
     
@@ -73,18 +77,21 @@ def valid_data():
     label = df['labels'].values.tolist()
     
     return valid_list, label
-   
+    
 if __name__ == '__main__':
+    
     train_list, train_label = train_data()
-    valid_list, valid_label = valid_data()
-    
-    train_set = get_data(train_list = train_list, label = train_label, transform = train_transform())
-    
+    train_set = get_data(data_list = train_list, label = train_label, transform = train_transform())
     train_loader = DataLoader(
-        dataset=train_set, batch_size=10, shuffle=False, num_workers=0) #shuffle=False
+        dataset=train_set, batch_size=10, shuffle=True, num_workers=0) #shuffle=False
     
+    valid_list, valid_label = valid_data()
+    valid_set = get_data(data_list = valid_list, label = valid_label, transform = valid_transform())
+    valid_loader = DataLoader(
+        dataset=valid_set, batch_size=10, shuffle=False, num_workers=0) #shuffle=False
     
-    
-    # for data in train_loader:
-    #     print('Size of image:', data[0].shape)  # batch_size * 3 * 300 * 300
+    # for i, data in enumerate(train_loader, 0):
+    #     print(i)
+    #     print(data[1].shape)
+        
         
